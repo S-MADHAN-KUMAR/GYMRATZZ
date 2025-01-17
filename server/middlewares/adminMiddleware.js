8fe0baf928c989ac946d994384672ada03070d29
Madhan Kumar <madhankumar4195@gmail.com>
2025-01-18 02:50:02 +0530

    Update adminMiddleware.js

--- server/middlewares/adminMiddleware.js
+++ server/middlewares/adminMiddleware.js
@@ -1,13 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 export const adminAuth = (req, res, next) => {
-  const token = req.cookies.ADMIN_TOKEN
+  // Extract the token from the Authorization header
+  const token = req.headers.authorization?.split(' ')[1];  // 'Bearer <token>'
+
+  console.log('Token extracted:', token);
 
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized: Token not found' })
+    return res.status(401).json({ message: 'Unauthorized: Token not found' });
   }
 
-
+  // Verify the JWT token
   jwt.verify(token, process.env.ADMIN_JWT_SECRET, (err, decoded) => {
     if (err) {
       if (err.name === 'TokenExpiredError') {
@@ -16,13 +19,7 @@ export const adminAuth = (req, res, next) => {
       return res.status(403).json({ message: 'Forbidden: Invalid token' });
     }
 
-    req.user = decoded;
-
-    const currentTime = Math.floor(Date.now() / 1000);
-    if (decoded.exp < currentTime) {
-      return res.status(403).json({ message: 'Forbidden: Token expired' });
-    }
-
+    // If emails match, proceed to the next middleware
     next();
   });
 };
