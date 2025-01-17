7d082af56a06d02c749d00aad2e7a79390d6dbf3
Madhan Kumar <madhankumar4195@gmail.com>
2025-01-17 23:52:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: process.env.FRONTEND_URL ,  // Frontend URL
+  origin: '*' ,  // process.env.FRONTEND_URL
   credentials: true,  // Allow cookies
 }));
 //======================config=========================//
