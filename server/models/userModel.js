ebf6f8c39b7b3a67618dcf5ed9729faad1088e36
Madhan Kumar <madhankumar4195@gmail.com>
2025-02-27 19:36:28 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String }, 
-  mobile: { type: Number, unique: true }, 
+  mobile: { type: Number, unique: true,default:'Not Added' }, 
   status: { type : Boolean , default:true },
   profilePicture: { type: String },
   otp: { type: Number }, 
